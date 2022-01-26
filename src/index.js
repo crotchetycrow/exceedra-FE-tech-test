1bfb273a8d38f13955edc2108dd036b3cb588f3e
Jack Hooper <77017281+crotchetycrow@users.noreply.github.com>
2022-01-26 16:19:41 +0000

    Log in styling partly complete & minimal JS functionality

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
-  const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+const toggleForm = document.querySelector('.reg-container');
 
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
 })
+
+toggleForm.classList.toggle('active');
