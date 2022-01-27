45bd80d1361b5a83afd595a2b8a5d93fabe91e24
Jack Hooper <77017281+crotchetycrow@users.noreply.github.com>
2022-01-27 17:55:58 +0000

    Testing JS functionality & input -> btn tag

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
-const toggleForm = document.querySelector('.reg-container');
+const toggleForm = document.querySelector('reg-container');
+
+const regBtn = document.getElementsByClassName('.register-btn');
 
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
@@ -10,3 +12,19 @@ toggleButton.addEventListener('click', () => {
 function setToggle() {
   toggleForm.classList.toggle('active');
 }
+
+regBtn.onclick = function () {
+  console.log('test')
+}
+
+// const regBtn = document.getElementsByTagName('input');
+// for(let i =0; i < regBtn.length; i++) {
+//   let sBtn = regBtn[i];
+//   if(sBtn.type == "submit" && sBtn.value == "Register") {
+//     sBtn.addEventListener('click', () => {
+//       console.log('Working')
+//     })
+//   }
+// }
+
+
