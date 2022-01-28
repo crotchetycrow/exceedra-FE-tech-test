12e349221460ef0e654f30ebbec39b9ee6899bb5
Jack Hooper <77017281+crotchetycrow@users.noreply.github.com>
2022-01-28 15:40:28 +0000

    Added data serialisation

--- src/index.js
+++ src/index.js
@@ -3,10 +3,41 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
 const toggleForm = document.querySelector('.reg-container');
 
+var form = document.querySelector('form');
+var data = new FormData(form);
+
+
+
+// Hamburger menu
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
 })
 
+// Form 'active'
 function setToggle() {
   toggleForm.classList.toggle('active');
 }
+
+// Serialising form data
+
+document.addEventListener('submit', function (event) {
+
+	event.preventDefault();
+
+	fetch('https://mock-members-api.herokuapp.com/members', {
+		method: 'POST',
+		body: JSON.stringify(Object.fromEntries(new FormData(event.target))),
+		headers: {
+			'Content-type': 'application/json; charset=UTF-8'
+		}
+	}).then(function (response) {
+		if (response.ok) {
+			return response.json();
+		}
+		return Promise.reject(response);
+	}).then(function (data) {
+		console.log(data);
+	}).catch(function (error) {
+		console.warn(error);
+	});
+});
