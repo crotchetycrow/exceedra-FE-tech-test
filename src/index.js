deb6b10b6343b5fd8f4dd6af324df55e4a5ec326
Jack Hooper <77017281+crotchetycrow@users.noreply.github.com>
2022-01-29 11:52:26 +0000

    Removed validateForm

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
 const toggleForm = document.querySelector('.reg-container');
 
-// var form = document.querySelector('form');
-// var data = new FormData(form);
+var form = document.querySelector('form');
+var data = new FormData(form);
 
 
 
@@ -20,34 +20,37 @@ function setToggle() {
 
 // Form validation for empty inputs
 
-function validateForm() {
-  var x = document.firstname.lastname.emailAddress.value;
-  if (x == "") {
-    alert("Name must be filled out");
-    return false;
-  }
-}
+// function validateForm() {
+//   var x = document.firstname.lastname.emailAddress.value;
+//   if (x == "") {
+//     alert("Name must be filled out");
+//     return false;
+//   }
+// }
 
-// Serialising form data
+// Serialising form data allowing it to console log
 
-// document.addEventListener('submit', function (event) {
+document.addEventListener('submit', function (event) {
 
-// 	event.preventDefault();
 
-// 	fetch('https://mock-members-api.herokuapp.com/members', {
-// 		method: 'POST',
-// 		body: JSON.stringify(Object.fromEntries(new FormData(event.target))),
-// 		headers: {
-// 			'Content-type': 'application/json; charset=UTF-8'
-// 		}
-// 	}).then(function (response) {
-// 		if (response.ok) {
-// 			return response.json();
-// 		}
-// 		return Promise.reject(response);
-// 	}).then(function (data) {
-// 		console.log(data);
-// 	}).catch(function (error) {
-// 		console.warn(error);
-// 	});
-// });
+  // This prevents page change, but without it data fails to fetch
+  // I think this is because there is no submit event on welcome.html
+	event.preventDefault();
+
+	fetch('https://mock-members-api.herokuapp.com/members', {
+		method: 'POST',
+		body: JSON.stringify(Object.fromEntries(new FormData(event.target))),
+		headers: {
+			'Content-type': 'application/json; charset=UTF-8'
+		}
+	}).then(function (response) {
+		if (response.ok) {
+			return response.json();
+		}
+		return Promise.reject(response);
+	}).then(function (data) {
+		console.log(data);
+	}).catch(function (error) {
+		console.warn(error);
+	});
+});
