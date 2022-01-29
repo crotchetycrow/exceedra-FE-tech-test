87d7f57c58cee285747b10e34c8fcf9da4249111
Jack Hooper <77017281+crotchetycrow@users.noreply.github.com>
2022-01-29 15:01:26 +0000

    Missing line

--- src/index.js
+++ src/index.js
@@ -36,6 +36,7 @@ fetch('https://mock-members-api.herokuapp.com/members')
 			table.classList.add('memDetails');
 			// DUE TO TIME CONSTRAINTS 
 			// The image is just hardcoded in - I was trying to grab the image from the API but I need to spend more time on it
+			// ${data[i].img} not working as expected
 			table.innerHTML =  `<tr><td><img src="https://img.icons8.com/cotton/64/000000/doctor-skin-type-3.png"></td><td>${data[i].fname}</td> <td>${data[i].lname}</td> <td>${data[i].email}</td> <td>${data[i].points}</td></tr>`;
 			
 			mainContainer.appendChild(table);
